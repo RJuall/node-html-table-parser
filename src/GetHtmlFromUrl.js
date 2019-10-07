8d62a460e4a590b3890294d8e6ed5397f7542eff
rjuall <rjuall@visuallease.com>
2019-10-07 16:23:32 -0400

    Rename tests add mocks

--- src/GetHtmlFromUrl.js
+++ src/GetHtmlFromUrl.js
@@ -3,10 +3,16 @@ const axios = require(`axios`);
 function getHtmlFromUrl(path) {
     axios.get(path)
       .then(res => {
-        return res.toString();
+        return res.data;
       })
       .catch(err => {
-
+          if (err.response) {
+            throw new Error(`HTTP Error Status - ${err.response.status}`);
+          } else if (err.request) {
+            throw new Error(`Error - ${err.request}`);
+          } else {
+            throw new Error(`Error - ${err.message}`);
+          }
       });
 }
 module.exports = getHtmlFromUrl;
