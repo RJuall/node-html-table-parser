7f95d78763dfbe2495e2c49966fe7a3d0e75b9e5
rjuall <rjuall@visuallease.com>
2019-10-08 12:37:40 -0400

    Clean up tests

--- __tests__/GetHtmlFromUrl.test.js
+++ __tests__/GetHtmlFromUrl.test.js
@@ -9,9 +9,11 @@ test(`Return html data given a well-formed request`, () => {
       .get(`/index.html`)
       .reply(200, `<html></html>`);
     
-    return getHtmlFromUrl(`https://www.fakewebsite.com/index.html`).then(data => {
-        expect(data).toBe(`<html></html>`);
-    });
+    return getHtmlFromUrl(`https://www.fakewebsite.com/index.html`)
+        .then(data => {
+            expect(data).toBe(`<html></html>`);
+        })
+        .catch(data => { throw new Error(`Test failed.`) });
 });
 
 test(`Return 404 given a non-existent URL`, () => {
@@ -20,8 +22,16 @@ test(`Return 404 given a non-existent URL`, () => {
       .reply(404);
 
     return getHtmlFromUrl(`https://www.fakewebsite.com/nothing.html`)
-        .then(data => {})
+        .then(data => { throw new Error(`Test failed.`) })
         .catch(data => {
             expect(data.toString()).toMatch(/404$/);
+        });
+});
+
+test(`Return 400 given invalid URL input`, () => {
+    return getHtmlFromUrl(`nonsense`)
+    .then(data => { throw new Error(`Test failed.`) })
+        .catch(data => {
+            expect(data.toString()).toMatch(/400$/);
         });
 });
