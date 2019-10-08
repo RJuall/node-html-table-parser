f7ca74f753741fc30d59d973068203993c104d3b
rjuall <rjuall@visuallease.com>
2019-10-08 15:56:16 -0400

    Create mock for fs module using mock-fs

--- __tests__/GetHtmlFromFilepath.test.js
+++ __tests__/GetHtmlFromFilepath.test.js
@@ -1,4 +1,21 @@
 const getHtmlFromFilepath = require(`../src/GetHtmlFromFilepath`);
+const mockFs = require(`mock-fs`);
+
+beforeEach(() => {
+    mockFs({
+        'C:\\temp\\test-fs': {
+            'html-file.html': '<html><p>this</p><p>that</p></html>',
+            'html-file-alt.htm': '<html><p>this</p><p>that</p></html>',
+            'html-txt.txt': '<html><p>this</p><p>that</p></html>',
+            'empty-file': '',
+            'directory-html': {
+                'html-file.html': '<html><p>this</p><p>that</p></html>',
+            },
+            'empty-directory': {},
+        }
+    });
+});
+afterEach(() => { mockFs.restore });
 
 test(`Placeholder test`, () => {
     expect(true).toBe(true);
