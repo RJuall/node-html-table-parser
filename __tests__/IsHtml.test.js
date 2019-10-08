082a4da4d9acf6a8a620a7e6fade252cf5830847
rjuall <rjuall@visuallease.com>
2019-10-08 15:13:57 -0400

    Write tests for HTML validation

--- __tests__/IsHtml.test.js
+++ __tests__/IsHtml.test.js
@@ -1,5 +1,25 @@
 const isHtml = require(`../src/IsHtml`);
 
-test(`Placeholder test`, () => {
-    expect(true).toBe(true);
-})
+test(`Return true given at least one valid HTML tag`, () => {
+    expect(
+        isHtml(`<p>`)
+    ).toBe(true);
+});
+
+test(`Return false given no valid HTML tags`, () => {
+    expect(
+        isHtml(`no tags here just text`)
+    ).toBe(false);
+});
+
+test(`Return true given at least one valid HTML tag and invalid tags`, () => {
+    expect(
+        isHtml(`<p></p>< p></p>`)
+    ).toBe(true);
+});
+
+test(`Return false given XML-style tags`, () => {
+    expect(
+        isHtml(`<nonsense></nonsense>`)
+    ).toBe(false);
+});
