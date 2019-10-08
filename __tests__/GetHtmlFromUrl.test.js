67654804178fa04fbe53aa763993bfb69973084c
rjuall <rjuall@visuallease.com>
2019-10-08 12:18:27 -0400

    Changed tests to use fake website

--- __tests__/GetHtmlFromUrl.test.js
+++ __tests__/GetHtmlFromUrl.test.js
@@ -5,21 +5,21 @@ afterAll(nock.restore);
 afterEach(nock.cleanAll);
 
 test(`Return html data given a well-formed request`, () => {
-    nock(`https://google.com`)
-      .get(`/`)
+    nock(`https://www.fakewebsite.com`)
+      .get(`/index.html`)
       .reply(200, `<html></html>`);
     
-    return getHtmlFromUrl(`https://google.com/`).then(data => {
+    return getHtmlFromUrl(`https://www.fakewebsite.com/index.html`).then(data => {
         expect(data).toBe(`<html></html>`);
     });
 });
 
 test(`Return 404 given a non-existent URL`, () => {
-    nock(`https://google.com/fake.html`)
-      .get(`/`)
+    nock(`https://www.fakewebsite.com`)
+      .get(`/nothing.html`)
       .reply(404);
 
-    return getHtmlFromUrl(`https://google.com/fake.html/`)
+    return getHtmlFromUrl(`https://www.fakewebsite.com/nothing.html`)
         .then(data => {})
         .catch(data => {
             expect(data.toString()).toMatch(/404$/);
