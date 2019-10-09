7767deb7299ccbcad810065dc588b10eaa95720c
rjuall <rjuall@visuallease.com>
2019-10-09 11:29:58 -0400

    Create SplitAllTablesIntoRowArrays fn

--- src/SplitAllTablesIntoRowArrays.js
+++ src/SplitAllTablesIntoRowArrays.js
@@ -1,4 +1,13 @@
+const splitTableStringIntoRowArray = require(`./SplitTableStringIntoRowArray`);
+
 function splitAllTablesIntoRowArrays(tableObj) {
     return true;
+
+    const splitTable = {};
+    for (let tableIndex in tableObj) {
+        splitTable[tableIndex] = 
+            splitTableStringIntoRowArray(tableObj[tableIndex]);
+    }
+    return splitTable;
 }
 module.exports = splitAllTablesIntoRowArrays;
