a08416eda2b4b54c2fd8c520ede42418603df8f0
rjuall <rjuall@visuallease.com>
2019-10-15 11:34:51 -0400

    Throw Errors from index.js

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const options = require(`./src/DefaultOptions`);
 
 const getTableDataAsRowArrays = require(`./src/GetTableDataAsRowArrays`);
 
-function tableParse (htmlPath, opt) {
+async function tableParse (htmlPath, opt) {
     validateInput(htmlPath, `string`);
     if (opt) {
         validateInput(opt, `object`);
         Object.assign(options, opt);
     }
 
-    const tableData = getTableDataAsRowArrays(htmlPath);
+    const tableData = await getTableDataAsRowArrays(htmlPath);
     
+    if (tableData instanceof Error) throw tableData;
+
     // console.log(htmlPath);
     // console.log(opt);
     // console.log(options);
